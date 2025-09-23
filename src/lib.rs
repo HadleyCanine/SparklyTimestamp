93a08b4144b1e4295cf577e19877a36e38341f2c
Hadley Canine <hadley@foxular.net>
2025-09-23 13:32:16 -0500

    Phew, it works now. I guess I should make a README to hold the associated autohotkey script.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,29 @@
-/*
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::ffi::CString;
+use std::os::raw::c_char;
+
+
+#[unsafe(no_mangle)]
+pub fn get_timestamp() -> *mut c_char {
+    use chrono::prelude::*;
+    let local: DateTime<Local> = Local::now();
+    let formatted = local.format("%Y-%m-%d %H:%M:%S (%z)").to_string();
+
+    let c_str = CString::new(formatted).unwrap();
+
+    // Give away the string's treasure map and tell Rust to forget about it
+    c_str.into_raw()
 }
 
+#[unsafe(no_mangle)]
+pub extern "C" fn free_timestamp(ptr: *mut c_char) {
+    if ptr.is_null() { return }
+    unsafe {
+        // Now we take back the treasure map and let Rust clean it up properly!
+        let _ = CString::from_raw(ptr);
+    }
+}
+
+/*
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -14,4 +35,3 @@ mod tests {
     }
 }
 */
-
